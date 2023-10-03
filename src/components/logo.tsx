e91ee3af6ab889a816e8c7a25369d4ca962d30cd
kaf-lamed-beyt <belac335@gmail.com>
2023-10-03 11:30:49 +0100

    feat: add new style props for improved customization

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,16 +4,45 @@ import { useTheme } from "next-themes";
 interface logoProps {
   dark: string;
   light: string;
+  size: string;
+  logoText?: string;
+  textSize?: string;
+  textWeight?: string;
 }
 
-const Logo = ({ dark, light }: logoProps) => {
+const Logo = ({
+  dark,
+  light,
+  size,
+  logoText,
+  textSize,
+  textWeight,
+}: logoProps) => {
   const { theme } = useTheme();
 
-  if (theme === "dark") {
-    return <img src={light} width="120" height="120" alt="dark theme logo" />;
-  }
+  // if (theme === "dark") {
+  //   return <img src={light} width={size} height={size} alt="dark theme logo" />;
+  // }
 
-  return <img src={dark} width="120" height="120" alt="light logo" />;
+  return (
+    <div
+      style={{ display: logoText ? "" : "flex", gap: logoText ? "" : ".6em" }}
+    >
+      <img
+        src={theme === "dark" ? light : dark}
+        width={size}
+        height={size}
+        alt={theme === dark ? "dark theme logo" : "light theme logo"}
+      />{" "}
+      {logoText ? (
+        <span
+          style={{ margin: "auto", fontSize: textSize, fontWeight: textWeight }}
+        >
+          {logoText}
+        </span>
+      ) : null}
+    </div>
+  );
 };
 
 export default Logo;
